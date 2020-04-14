e7736526e29e710d5c129c46fd8ae9091f87376c
Stuart Lynn <stuart.lynn@gmail.com>
2020-04-14 16:54:32 -0400

    Updates

---
+++ precache-manifest.3b6483ccd4aeab0baba895bedb9950de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8a24479ed73de66d90821da43dfc7b",
+    "url": "/WordEmbedVis/index.html"
+  },
+  {
+    "revision": "c64a811fdb0315ddb209",
+    "url": "/WordEmbedVis/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "750cf0a15650125f1216",
+    "url": "/WordEmbedVis/static/js/2.33126b0d.chunk.js"
+  },
+  {
+    "revision": "c64a811fdb0315ddb209",
+    "url": "/WordEmbedVis/static/js/main.c35faf29.chunk.js"
+  },
+  {
+    "revision": "f0b079b127d802df07e4",
+    "url": "/WordEmbedVis/static/js/runtime-main.d6967407.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/WordEmbedVis/static/media/logo.25bf045c.svg"
+  }
+]);
